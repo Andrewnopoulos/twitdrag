527e6765127ddc613d6bd1a49590061b3bbcd707
Andrew Giannopoulos <andrewgi@blackmagicdesign.com>
2020-09-09 20:45:21 +1000

    use urlencode and urlunparse to properly generate the http request

--- src/core.py
+++ src/core.py
@@ -1,4 +1,5 @@
 import urllib.request
+from urllib.parse import urlencode, urlunparse
 from html.parser import HTMLParser
 import ssl
 import json
@@ -13,38 +14,36 @@ class tweetHTMLParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
 		if attrs:
 			for attr in attrs:
-				if attr[0] == 'data-conversation-id' and tag == 'a':
+				if attr[0] == 'data-tweet-id':
 					self.activeTweetId = attr[1]
+					self.tweetDict[self.activeTweetId] = {'timestamp': -1, 'text': ""}
 				if attr[0] == 'data-time':
-					self.tweetDict[self.activeTweetId] = {'timestamp': attr[1]}
+					self.tweetDict[self.activeTweetId]['timestamp'] = attr[1]
 
 			if attrs[0][1] and 'js-tweet-text tweet-text' in attrs[0][1]:
 				self.appendingText = True
 
 	def handle_endtag(self, tag):
 		if tag == 'p':
-			if self.activeTweetId is not -1 and self.tweetDict[self.activeTweetId]['text']:
+			if self.activeTweetId is not -1:
 				self.activeTweetId = -1
 				self.appendingText = False
 
 	def handle_data(self, data):
-		if self.appendingText:
-			if 'text' in self.tweetDict[self.activeTweetId]:
-				if 'pic.twitter.com' in data:
-					self.tweetDict[self.activeTweetId]['text'] += ' '
-				self.tweetDict[self.activeTweetId]['text'] += data
-			else:
-				self.tweetDict[self.activeTweetId]['text'] = data
+		if self.appendingText and self.activeTweetId is not -1:
+			if 'pic.twitter.com' in data:
+				self.tweetDict[self.activeTweetId]['text'] += ' '
+			self.tweetDict[self.activeTweetId]['text'] += data
 
 def getJsonDataFromUser(userAccount):
-	# baseUrl = 'https://twitter.com/i/search/timeline?f=realtime&q=(from%3Acodeandbeard)&src=typed_query&f=live'
-	baseUrl = 'https://twitter.com/i/search/timeline?'
 	fromItem = '(from:{})'.format(userAccount)
-	parameters = {'f': 'realtime', 'last_note_ts': '85', 'q': fromItem, 'src': 'typd', 'f': 'live'}
-	url = baseUrl
-	for key in parameters:
-		url += key + '=' + parameters[key] + '&'
-	url = url[:-1]
+	params = {
+		'f': 'tweets',
+		'q': fromItem
+	}
+	url_tuple = ('https', 'twitter.com', '/i/search/timeline', '', urlencode(params), '')
+	url = urlunparse(url_tuple)
+	print(url)
 	headers = {'user-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 	gcontext = ssl.SSLContext()
 	r = urllib.request.Request(url, headers=headers)
